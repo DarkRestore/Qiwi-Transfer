70516d956409814273902816bde8664813d74017
DarkRestore <57294765+DarkRestore@users.noreply.github.com>
2021-10-04 09:40:35 +0300

    Create QiwiTransfer.py

---
+++ QiwiTransfer.py
@@ -0,0 +1,8 @@
+import time
+
+print("[*] - QIWI SERVER MONEY TRANSFER - [*]")
+print("[+] - Loading servers ")
+print("[+] - Start ")
+print("[+] - Sending: 126 000,00RUB to [+79642380940] ")
+print("[-] - Sending failed ")
+print("[!] - Your server did not fit ")
